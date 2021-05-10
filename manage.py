be2a0b875a1fa5906ca17ecd86e75c42998c7941
Valentin <cochin.valentin@hotmail.fr>
2021-05-10 09:09:24 +0200

    Update manage.py: can run tests with coverage

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import unittest
 
+import coverage as coverage
 from flask.cli import FlaskGroup
 
 from project import create_app
@@ -7,6 +8,15 @@ from project import create_app
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 @cli.command()
 def test():
@@ -17,6 +27,20 @@ def test():
         return 0
     return 1
 
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
